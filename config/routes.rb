c0dcf36e87e70efae332eaf7cdf4077de3136182
WataruT <takahashi@ohyachi-hp.or.jp>
2019-06-03 14:13:48 +0900

    職員検索ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get 'search/home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'search#home'
 end
